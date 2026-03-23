cdd8296dc6d74b2ef90431ba1339b7532c1f94c6
Alex Inch <ucabaj7@ucl.ac.uk>
2026-03-23 16:16:08 +0000

    some more dev bits for MPC

--- assignment_3_2026/controller_alex_mpc.py
+++ assignment_3_2026/controller_alex_mpc.py
@@ -1,14 +1,51 @@
 # wind_flag = False
 # Implement a controller
+# %%
+from functools import cache
+
 import numpy as np
 import qpsolvers
 
+dt = 0.02
+
 
 class MPCController:
-    def __init__(self):
+    def __init__(self, horizon: int = 10):
+        self.horizon = horizon
         # define the model
 
-        return None
+        I = np.eye(4)
+        C = np.array([1, 0])  # only emit
+        self.C = np.kron(C, I)
+        return
+
+    @staticmethod
+    @cache  # cache so we're not recomputing unnecessarily
+    def _get_A(dt: float):
+        """Encodes p_kt = p_k + dt * v_k. This is a function so we can handle changing dt"""
+        A = np.array(
+            [
+                [1, dt],
+                [0, 1],
+            ]
+        )
+        return np.kron(A, np.eye(4))
+
+    @staticmethod
+    @cache
+    def _get_B(dt: float):
+        B = np.array(
+            [
+                [0],
+                [dt],
+            ]
+        )
+        return np.kron(B, np.eye(4))
+
+    def kalman(self, state: np.ndarray) -> np.ndarray:
+        return state
+
+    def mpc(self, x: np.ndarray, t: np.ndarray, dt: np.ndarray) -> np.ndarray: ...
 
     def __call__(
         self,
@@ -16,7 +53,9 @@ class MPCController:
         target: np.ndarray,
         dt: np.ndarray,
     ) -> np.ndarray:
-        return state
+        state_estimate = self.kalman(state)
+        control = self.mpc(state_estimate, target, dt)
+        return control
 
 
 def get_rot_matrix(theta):
@@ -24,6 +63,7 @@ def get_rot_matrix(theta):
 
 
 mpc_controller = MPCController()
+# %%
 
 
 def controller(state, target_pos, dt, wind_enabled=False):
@@ -42,4 +82,5 @@ def controller(state, target_pos, dt, wind_enabled=False):
     theta = state[3]
     control[0:2] = get_rot_matrix(theta) @ control[0:2]
 
+    output = None
     return output
