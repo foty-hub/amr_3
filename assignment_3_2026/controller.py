1e9c41a67bfe8cbe392f71f90dc73a94171a2aa6
Ben <bew.woodruff@gmail.com>
2026-04-27 12:43:49 +0100

    Updates Controller.py

--- assignment_3_2026/controller.py
+++ assignment_3_2026/controller.py
@@ -1,10 +1,48 @@
 # wind_flag = False
 # Implement a controller
 from collections import deque
+import csv
+import time
+from pathlib import Path
 
 import numpy as np
 
-from save_data import save_data
+
+def save_data(
+    dt: float,
+    state: np.ndarray,
+    target: np.ndarray,
+    control_output: np.ndarray,
+    wind_enabled: bool,
+    output_file: str,
+):
+    row = dict(
+        recording_time_ns=time.time_ns(),
+        dt=dt,
+        wind_enabled=wind_enabled,
+        pos_x=state[0],
+        pos_y=state[1],
+        pos_z=state[2],
+        pos_roll=state[3],
+        pos_pitch=state[4],
+        pos_yaw=state[5],
+        targetpos_x=target[0],
+        targetpos_y=target[1],
+        targetpos_z=target[2],
+        targetpos_yaw=target[3],
+        control_vel_x=control_output[0],
+        control_vel_y=control_output[1],
+        control_vel_z=control_output[2],
+        control_vel_yaw=control_output[3],
+    )
+
+    file_exists = Path(output_file).is_file()
+
+    with open(output_file, mode="a", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=row.keys())
+        if not file_exists:
+            writer.writeheader()
+        writer.writerow(row)
 
 
 class PIDController:
@@ -14,18 +52,24 @@ class PIDController:
         self.history = deque(maxlen=100)
         self.last_target = None
         
-        self.KGain = 2.5
-        self.IGain = 0.05
-        self.DGain = 0.9
-        self.Kp = np.array([self.KGain,self.KGain,self.KGain, 5])
+        self.KGain = 0.6
+        self.IGain = 0.0001
+        self.DGain = 0.3
+        self.Kp = np.array([self.KGain,self.KGain,self.KGain, 0.6])
         self.Ki = np.array([self.IGain,self.IGain,0,0])
         self.Kd = np.array([self.DGain,self.DGain, 0.00, 0.0])
+        self.prev_dt = time.time()
 
     def __call__(self, state, target_pos, dt):
         if self._target_changed(target_pos):
             self.reset()
         self.last_target = np.array(target_pos, copy=True)
 
+        if dt > 1e5:
+            temp = dt / pow(10,3)
+            dt = (dt - self.prev_dt) /  pow(10,3)
+            self.prev_dt = temp
+            
         err = target_pos - state
         # wrap the angular error into [-pi, +pi]
         err[3] = (err[3] + np.pi) % (2 * np.pi) - np.pi
@@ -81,9 +125,9 @@ def controller(state, target_pos, dt, wind_enabled=False):
 
     #output = (control[0], control[1], control[2], control[3])
     output = (
-                    np.clip(control[0], -1, 1),
-                    np.clip(control[1], -1, 1),
-                    np.clip(control[2], -1, 1),
+                    np.clip(control[0], -0.3, 0.3),
+                    np.clip(control[1], -0.3, 0.3),
+                    np.clip(control[2], -0.3, 0.3),
                     np.clip(control[3], -1.74533, 1.74533),
     )
     
